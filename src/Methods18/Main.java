492afe1c3849213067ca18e5a9230f394e9b16dc
elefantaka <reniawiatr@gmail.com>
2018-08-14 22:20:30 +0200

    third commit

--- src/Methods18/Main.java
+++ src/Methods18/Main.java
@@ -10,8 +10,13 @@ public class Main {
 	 */
 
 		Test a = new Test(); //obiekt
-
 		a.write("Renata", "Wietrzynska"); //wywolanie metody
+
+		Test a2 = new Test(); //obiekt
+		a2.write("Michal", "Wietrzynski"); //wywolanie metody
+
+		double result = a.division(10,0);
+		System.out.println(result);
 	}
 
 }
@@ -20,7 +25,22 @@ class Test //klasa
 {
 	void write(String name, String surname) //metoda
 	{
-		System.out.println(name + " " + surname);
+		System.out.println("name: " + name);
+		System.out.println("surname: " + surname);
+
+	}
+
+	int add(int a, int b)
+	{
+		return a + b;
+	}
+
+	double division(double a, double b)
+	{
+		if (b == 0)
+			return 0;
+		System.out.println("lala");
+		return a / b;
 	}
 }
 //6.32 - minuta
